c5615ed57e399122dda3ad44c14d2beea31c880e
nkundur27 <nikhilkundur9@gmail.com>
2025-11-03 20:30:42 +0530

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js');
 });
 
-app.listen(5000, '0.0.0.0', () => {
-  console.log(`Server running at http:// 13.203.103.17:5000`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log(`Server running at http:// 13.203.103.17:3000`);
 });
 

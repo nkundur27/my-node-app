ae9a00517c279248abbe13f96bd439986e9c1dc0
nkundur27 <nikhilkundur9@gmail.com>
2025-11-03 20:51:25 +0530

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
 
 app.get('/', (req, res) => {
   res.send('Hello from Node.js');
 });
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log('Server running at http://localhost:${PORT}');
 });
 
